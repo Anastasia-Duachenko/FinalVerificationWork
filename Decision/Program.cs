ed53dd42561ad08c379a2bbc69ede249943455b1
Anastasia-Duachenko <124395601+Anastasia-Duachenko@users.noreply.github.com>
2023-02-15 23:17:35 +0800

    Add solition
    Last update

---
+++ Decision/Program.cs
@@ -0,0 +1,42 @@
+﻿
+Console.Clear();
+
+string[] ReferToArray() 
+{
+    Console.Write("Введите значения через запятую: ");
+    return Console.ReadLine().Split(",");
+}
+
+string[] array = ReferToArray();
+Console.WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", SearchingForValues(array, 3))}]");
+
+string[] SearchingForValues(string[] entry, int t) 
+{
+    string[] exit = new string[NumberOfValues(entry, t)];
+    for(int i = 0, j = 0; i < entry.Length; i++) 
+    {
+        if(entry[i].Length <= t) 
+        {
+            exit[j] = entry[i];
+            j++;
+        }
+    }
+
+    return exit;
+}
+
+int NumberOfValues(string[] entry, int t) 
+{
+    int count = 0;
+    for(int i = 0; i < entry.Length; i++) 
+    {
+        if(entry[i].Length <= t) 
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+
